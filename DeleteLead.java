f0bf66bd2df7557db5f6b06409ebfaebfc1aea10
ZAditi <105577087+ZAditi@users.noreply.github.com>
2022-05-28 09:19:04 +0530

    Issue in Duplicate Lead and Delete Lead

---
+++ DeleteLead.java
@@ -0,0 +1,42 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DeleteLead {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.linkText("Phone")).click();
+		driver.findElement(By.name("phoneCountryCode")).clear();
+		driver.findElement(By.name("phoneCountryCode")).sendKeys("91");
+		driver.findElement(By.name("phoneNumber")).sendKeys("5868324354");
+		driver.findElement(By.linkText("Find Leads")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).getText();
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.findElement(By.linkText("Delete")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.linkText("Phone")).click();
+		driver.findElement(By.name("phoneCountryCode")).clear();
+		driver.findElement(By.name("phoneCountryCode")).sendKeys("91");
+		driver.findElement(By.name("phoneNumber")).sendKeys("5868324354");
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.linkText("No records to display")).getText();
+		driver.close();
+		
+		
+	}
+
+}
